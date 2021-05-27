a12afdd847de0da10e146d06c87a55a437e71ca1
nibinbaiju <nibinbaiju@gmail.com>
2021-05-27 12:34:38 +0530

    msgggg

--- resources/views/veruthe.blade.php
+++ resources/views/veruthe.blade.php
@@ -6,7 +6,7 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>REDIRECT</title>
-  
+
   <style>
 
 body, html {
@@ -53,7 +53,7 @@ h1{
 <body>
 <nav class="navbar navbar-expand-lg  navbar-light bg-secondary">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Studena</a>
+    <a class="navbar-brand" href="#">E-POCKET</a>
     <button
       class="navbar-toggler"
       type="button"
